01d42f9135924f48a04f58c60703515f572f6602
FranORT <54158944+FranORT@users.noreply.github.com>
2019-10-26 19:21:53 -0300

    Agregadas rutas y firmas
    Agrego firma de métodos en heroController.js, agregadas rutas a heroes.routes.js

--- app/routes/heroes.routes.js
+++ app/routes/heroes.routes.js
@@ -3,6 +3,10 @@ module.exports = (app) => {
 
 	app.post('/heroes', heroes.create);
 
-	app.get('/heroes/list', heroes.findAll);
+	app.get('/heroes/list', heroes.getAll);
 
-	
+	app.get('/heroes/:heroId', heroes.get);
+
+	app.put('/heroes/:heroId', heroes.update);
+
+	app.delete('/heroes/:heroId', heroes.delete);
