2f12befa0326f90ecb4dc2c04652dd11155aacb3
Damian Kravetz <kravetzdamian@gmail.com>
2019-10-30 21:55:32 -0300

    her changes

---
+++ src/entities/heroType.js
@@ -0,0 +1,10 @@
+
+const HeroType = Object.freeze({
+    "knight":{defense: 30, attack: 20, health: 220}, 
+    "mage":{defense: 30, attack: 20, health: 200}, 
+    "ranger":{defense: 30, attack: 20, health: 190}});
+
+module.exports = HeroType;
+
+
+
